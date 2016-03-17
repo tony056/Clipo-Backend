45645a6555ee8863cddd2f43745dfbdc41e2334f
tony056 <tony61507@gmail.com>
2016-03-17 16:56:12 +0800

    Removed Topic and background action to other files

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,6 @@
 var constants = require('cloud/constants.js');
 var utils = require('cloud/utils.js');
+var topic = require('cloud/topic.js');
 
 
 Parse.Cloud.afterSave(constants.CLIPO_LOG, function(request){
@@ -62,30 +63,14 @@ Parse.Cloud.afterSave(constants.CLIPO_PROJECT, function(request){
 	});
 });
 
+/* deleted project */
+
+
+
 /* saved comment */
 Parse.Cloud.afterSave(constants.CLIPO_COMMENT, function(request){
 
 });
 
-/* saved topic */
-Parse.Cloud.afterSave(constants.CLIPO_TOPIC, function(request){
 
-});
-
-
-
-/* send to pubnub */
-Parse.Cloud.define(constants.SEND_TO_PUBNUB, function(request, response){
-	Parse.Cloud.httpRequest({
-		url: 'http://pubsub.pubnub.com/publish/' + constants.pubnubConfiguration.publish_key + '/' 
-		+ constants.pubnubConfiguration.subscribe_key + '/0/' + request.params.channel + 
-		 '/0/' + encodeURIComponent(JSON.stringify(request.params.message)),
-		success: function(httpResponse){
-			response.success(httpResponse);
-		},
-		error: function(httpResponse){
-			response.error(httpResponse);
-		}
-	});
-});
 
