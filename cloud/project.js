1402cb6c4c657c7eb67d662bf9340e078032573a
tony056 <tony61507@gmail.com>
2016-03-18 00:47:27 +0800

    Finished project save/update part

--- cloud/project.js
+++ cloud/project.js
@@ -8,14 +8,15 @@ Parse.Cloud.afterSave(constants.CLIPO_PROJECT, function(request){
 	var projectInfo = request.object;
 	var projectId = projectInfo.id;
 	projectInfo.get('owner').fetch().then(function(owner){
+		var action = utils.checkACType();
 		var info = {
 			ownerName: owner.get('username'),
-			ownerId: owner.get('objectId'),
+			ownerId: owner.id,
 			targetName: projectInfo.get('name'),
 			targetType: constants.CLIPO_PROJECT,
 			projectId: projectId,
-			action: constants.ADD
-		}
+			action: action
+		};
 		utils.saveLog(info, function(err, result){
 			if(err) throw err;
 			console.log(result);
