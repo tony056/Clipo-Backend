4c6ae6280f24ee8eb7249acea0cf02f967afb0ca
tony056 <tony61507@gmail.com>
2016-03-18 00:50:59 +0800

    added methods for test program

--- cloud/utils.js
+++ cloud/utils.js
@@ -25,6 +25,46 @@ module.exports.saveLog = function(info, callback){
 	});
 };
 
+module.exports.setLogInfo = function(owner, target, targetType, projectId){
+	if(owner === null)
+		return null;
+	if(projectId === null)
+		return null;
+	var action = checkACType(target);
+	var info = {
+		ownerName : owner.get('username'),
+		ownerId : owner.id,
+		targetName : target.get('name'),
+		targetType : targetType,
+		action : action,
+		projectId : projectId
+	};
+	return info;
+};
+
+module.exports.queryObject = function(type, objectId, callback){
+	var query = new Parse.Query(type);
+	query.get(objectId, {
+		success: function(object){
+			callback(null, object);
+		},
+		error: function(error) {
+			callback(error.message, null);
+		}
+	});
+};
+
+var checkACType = function(object){
+	var action = constants.NO_ACTION;
+	if(object.existed())
+		action = constants.REVISE;
+	else
+		action = constants.ADD;
+	return action;
+};
+
+
+
 
 /* send to pubnub */
 Parse.Cloud.define(constants.SEND_TO_PUBNUB, function(request, response){
